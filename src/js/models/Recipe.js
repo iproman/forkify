9f2f3df477a6e0df2228d74ec66d5cda7d6db6b0
iproman <itechman@yandex.ru>
2020-09-07 14:36:09 +0500

    Parse ingredients. Short units.

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -43,4 +43,9 @@ export default class Recipe {
     calcServings() {
         this.servings = 4;
     }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+    }
 }
