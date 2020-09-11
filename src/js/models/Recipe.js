15ea3d07b686857b468a79e8cf7aa82de36ca312
iproman <itechman@yandex.ru>
2020-09-11 09:58:17 +0500

    Update servings/ingredients in recipe.

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -110,4 +110,21 @@ export default class Recipe {
             return objIng;
         });
     }
+
+    /**
+     * Update servings.
+     * @param type
+     */
+    updateServings(type) {
+
+        // Servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+
+        // Ingredients
+        this.ingredients.forEach(el => {
+            el.count *= (newServings / this.servings);
+        });
+
+        this.servings = newServings;
+    }
 }
